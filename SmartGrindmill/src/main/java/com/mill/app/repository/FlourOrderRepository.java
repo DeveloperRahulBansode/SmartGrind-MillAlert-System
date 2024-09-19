6356a034a63d37a6e09452f0fbbdc7d4eb16f38d
RAHUL BABA BANSODE <rahulprofessionalid@gmail.com>
2024-09-19 17:24:23 +0530

    Delete SmartGrindmill directory

--- SmartGrindmill/src/main/java/com/mill/app/repository/FlourOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mill.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.mill.app.model.FlourOrder;
-
-public interface FlourOrderRepository extends CrudRepository<FlourOrder, Long> {
-}
